65dd11aefcaeab72739b3e3f4e9cfcf5cee0ddac
UDAY-05-PRIME <udaykumawat214125@gmail.com>
2024-12-11 19:57:27 +0530

    updating code

--- Quiz-game/script.js
+++ Quiz-game/script.js
@@ -4,26 +4,26 @@ const questions = [
     {
         question: "Who directed the Interstellar movie?",
         answers: [
-            { text: "A. Christopher Nolan", correct: true },
-            { text: "B. Steven Spielberg", correct: false },
-            { text: "C. Quentin Tarantino", correct: false },
-            { text: "D. Martin Scorsese", correct: false },
+            { text: "A. Steven Spielberg", correct: false },
+            { text: "B. Quentin Tarantino", correct: false },
+            { text: "C. Martin Scorsese", correct: false },
+            { text: "D. Christopher Nolan", correct: true },
         ]
     },
     {
         question: "Which movie won the Academy Award for Best Picture in 1994?",
         answers: [
-            { text: "A. Pulp Fiction", correct: false },
-            { text: "B. Forrest Gump", correct: true },
-            { text: "C. The Shawshank Redemption", correct: false },
-            { text: "D. Braveheart", correct: false },
+            { text: "A. Forrest Gump", correct: true },
+            { text: "B. Pulp Fiction", correct: false },
+            { text: "C. Braveheart", correct: false },
+            { text: "D. The Shawshank Redemption", correct: false },
         ]
     },
     {
         question: "Which actor played the character of Jack Dawson in Titanic?",
         answers: [
-            { text: "A. Leonardo DiCaprio", correct: true },
-            { text: "B. Brad Pitt", correct: false },
+            { text: "A. Brad Pitt", correct: false },
+            { text: "B. Leonardo DiCaprio", correct: true },
             { text: "C. Matt Damon", correct: false },
             { text: "D. Johnny Depp", correct: false },
         ]
@@ -31,10 +31,10 @@ const questions = [
     {
         question: "What is the highest-grossing movie of all time as of 2024?",
         answers: [
-            { text: "A. Avatar", correct: false },
-            { text: "B. Avengers: Endgame", correct: true },
+            { text: "A. Avengers: Endgame", correct: true },
+            { text: "B. Star Wars: The Force Awakens", correct: false },
             { text: "C. Titanic", correct: false },
-            { text: "D. Star Wars: The Force Awakens", correct: false },
+            { text: "D. Avatar", correct: false },
         ]
     },
     {
@@ -49,17 +49,17 @@ const questions = [
     {
         question: "Which movie features the quote 'I'll be back'?",
         answers: [
-            { text: "A. The Terminator", correct: true },
-            { text: "B. Predator", correct: false },
-            { text: "C. Total Recall", correct: false },
+            { text: "A. Predator", correct: false },
+            { text: "B. Total Recall", correct: false },
+            { text: "C. The Terminator", correct: true },
             { text: "D. Commando", correct: false },
         ]
     },
     {
         question: "Who played the character of 'The Joker' in The Dark Knight (2008)?",
         answers: [
-            { text: "A. Joaquin Phoenix", correct: false },
-            { text: "B. Heath Ledger", correct: true },
+            { text: "A. Heath Ledger", correct: true },
+            { text: "B. Joaquin Phoenix", correct: false },
             { text: "C. Jack Nicholson", correct: false },
             { text: "D. Jared Leto", correct: false },
         ]
@@ -67,8 +67,8 @@ const questions = [
     {
         question: "Which movie is based on the Marvel superhero Iron Man?",
         answers: [
-            { text: "A. Iron Man", correct: true },
-            { text: "B. Spider-Man", correct: false },
+            { text: "A. Spider-Man", correct: false },
+            { text: "B. Iron Man", correct: true },
             { text: "C. Captain America", correct: false },
             { text: "D. Thor", correct: false },
         ]
@@ -76,16 +76,16 @@ const questions = [
     {
         question: "Who directed the movie 'Jurassic Park'?",
         answers: [
-            { text: "A. Steven Spielberg", correct: true },
+            { text: "A. George Lucas", correct: false },
             { text: "B. James Cameron", correct: false },
-            { text: "C. George Lucas", correct: false },
+            { text: "C. Steven Spielberg", correct: true },
             { text: "D. Ridley Scott", correct: false },
         ]
     },
     {
         question: "Which movie is considered the first feature-length animated film?",
         answers: [
-            { text: "A. Snow White and the Seven Dwarfs", correct: true },
+            { text: "A. Snow White", correct: true },
             { text: "B. Pinocchio", correct: false },
             { text: "C. Fantasia", correct: false },
             { text: "D. Cinderella", correct: false },
@@ -103,8 +103,8 @@ const questions = [
     {
         question: "Which movie features the song 'My Heart Will Go On'?",
         answers: [
-            { text: "A. Titanic", correct: true },
-            { text: "B. The Bodyguard", correct: false },
+            { text: "A. The Bodyguard", correct: false },
+            { text: "B. Titanic", correct: true },
             { text: "C. Pretty Woman", correct: false },
             { text: "D. Ghost", correct: false },
         ]
@@ -112,51 +112,43 @@ const questions = [
     {
         question: "Which film is known as the first movie in the Marvel Cinematic Universe?",
         answers: [
-            { text: "A. Iron Man", correct: true },
-            { text: "B. The Incredible Hulk", correct: false },
-            { text: "C. Captain America: The First Avenger", correct: false },
-            { text: "D. Thor", correct: false },
+            { text: "A. The Incredible Hulk", correct: false },
+            { text: "B. Captain America", correct: false },
+            { text: "C. Thor", correct: false },
+            { text: "D. Iron Man", correct: true },
         ]
     },
     {
         question: "Who directed the movie 'The Godfather'?",
         answers: [
-            { text: "A. Francis Ford Coppola", correct: true },
-            { text: "B. Martin Scorsese", correct: false },
-            { text: "C. Brian De Palma", correct: false },
-            { text: "D. Stanley Kubrick", correct: false },
+            { text: "A. Brian De Palma", correct: false },
+            { text: "B. Stanley Kubrick", correct: false },
+            { text: "C. Francis Ford Coppola", correct: true },
+            { text: "D. Martin Scorsese", correct: false },
         ]
     },
-    {
-        question: "Which movie won the Academy Award for Best Picture in 2023?",
-        answers: [
-            { text: "A. Everything Everywhere All At Once", correct: true },
-            { text: "B. Top Gun: Maverick", correct: false },
-            { text: "C. The Banshees of Inisherin", correct: false },
-            { text: "D. Avatar: The Way of Water", correct: false },
-        ]
-    }
 ];
-    let currentQuestionindex = 0;
+
+    let currentQuestionIndex = 0;
     let score = 0;
 
     const questionElement = document.getElementById("Question");
     const answerButtons = document.querySelectorAll(".options button");
     const questionNumberElement = document.querySelector(".questionNumber");
     const nextButton = document.getElementById("nextBtn");
+    const scoreElement = document.querySelector(".scoreButton");
 
     function startQuiz(){
-        currentQuestionindex = 0;
+        currentQuestionIndex = 0;
         score = 0;
         nextButton.innerHTML = "Next";
         loadQuestion();
-
     }
 
     function loadQuestion(){
         resetState();
-        const currentQuestion = questions[currentQuestionindex];
-        let questionNo = currentQuestionindex + 1;
+        const currentQuestion = questions[currentQuestionIndex];
+        let questionNo = currentQuestionIndex + 1;
         questionElement.innerText = questionNo + ". " + currentQuestion.question;
 
         answerButtons.forEach((button, index) => {
@@ -167,26 +159,41 @@ const questions = [
                 button.dataset.correct = answer.correct;
             
             button.classList.remove("correct", "incorrect");
-            button.onclick = selectAnswer;
+            button.onclick = selectAnswer; 
         });
     }
     
     function resetState(){
         nextButton.style.display = "none";
+        answerButtons.forEach(button =>{
+            button.disabled = false;
+        });
     }
     
     function selectAnswer(e){
-        console.log("function run");
         const selectedBtn = e.target;
         const isCorrect = selectedBtn.dataset.correct === "true";
         if(isCorrect){
             selectedBtn.classList.add("correct");
-            console.log("right");
+            score = score+10;
+            scoreElement.innerText = "Score: " + score;
         }
         else{
             selectedBtn.classList.add("incorrect");
-            console.log("wrong");
         }
-        console.log("function run 2");
+        Array.from(answerButtons).forEach(button =>{
+            if(button.dataset.correct === "true"){
+                button.classList.add("correct");
+            }
+            button.disabled = true;
+        });
+        nextButton.style.display = "block"
     }
+    function handleNextButton(){
+        currentQuestionIndex++;
+        if(currentQuestionIndex < questions.length){
+            loadQuestion();
+        }
+    }
+    nextButton.onclick = handleNextButton;
     startQuiz();
